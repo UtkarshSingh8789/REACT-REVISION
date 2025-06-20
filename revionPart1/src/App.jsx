a20aec2eb714853a01f6479d584ddc8e3a3b1e8b
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-20 21:00:20 +0530

    concept of passing data from lca to the child component

--- revionPart1/src/App.jsx
+++ revionPart1/src/App.jsx
@@ -100,24 +100,36 @@
 
 // USEPREV HOOK -> CUSTOM HOOKS;
 
-import React, { useState } from "react";
-import usePrev from "../customHooks/usePrev.jsx";
-function App() {
-  const [count,setCount]=useState(0);
-  const prevCount=usePrev(count);// let me explain how this work first usePrev(0) we know that in react first there will return then effect will call;
-  // hence for usePrev(0) return null then effect will call and ref.current=1 now if we increase count app component rerender and usePrev(1)
-  // first return then return ref.current=0 and now effect will run ref.current=1 now .. similarly working;
-  // note why we use dependecies in useEffect in usePrev custom hook because if we not use then if there is change in value then useEffect will not run because it doesnot have any dependencies;
-  // to make useEffect run on every change of value useEffect should depend upon value;
+// import React, { useState } from "react";
+// import usePrev from "../customHooks/usePrev.jsx";
+// function App() {
+//   const [count,setCount]=useState(0);
+//   const prevCount=usePrev(count);// let me explain how this work first usePrev(0) we know that in react first there will return then effect will call;
+//   // hence for usePrev(0) return null then effect will call and ref.current=1 now if we increase count app component rerender and usePrev(1)
+//   // first return then return ref.current=0 and now effect will run ref.current=1 now .. similarly working;
+//   // note why we use dependecies in useEffect in usePrev custom hook because if we not use then if there is change in value then useEffect will not run because it doesnot have any dependencies;
+//   // to make useEffect run on every change of value useEffect should depend upon value;
+//   return (
+//     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+//       <h1>Counter with usePrev Hook</h1>
+//       <p>Current Count: {count}</p>
+//       <p>Previous Count: {prevCount}</p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//       <button onClick={() => setCount(count - 1)} style={{ marginLeft: '10px' }}>Decrement</button>
+//     </div>
+//   )
+// }
+// export default App;
+
+
+import Parent from "../contextApi/rollingUpTheStates.jsx";
+function App(){
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Counter with usePrev Hook</h1>
-      <p>Current Count: {count}</p>
-      <p>Previous Count: {prevCount}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)} style={{ marginLeft: '10px' }}>Decrement</button>
+    <div>
+      <Parent/>
     </div>
   )
 }
 export default App;
+
 
